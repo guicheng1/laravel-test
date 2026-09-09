66d2bcc393cabe283750446bd427eccd12af9a77
poper-huangdong <dong.huang@poper.co>
2026-09-09 13:48:14 +0800

    Update title version from v1 to v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}-v1</title>
+        <title>{{ config('app.name', 'Laravel') }}-v2</title>
 
         @fonts
 
