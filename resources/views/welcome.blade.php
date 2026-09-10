ce78a5d22a01c83515859c88394535765de3ebaa
poper-huangdong <dong.huang@poper.co>
2026-09-10 21:27:27 +0800

    Update page title version from v3 to v4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}-v3</title>
+        <title>{{ config('app.name', 'Laravel') }}-v4</title>
 
         @fonts
 
