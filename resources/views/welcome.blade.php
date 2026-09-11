1d8fc4cf6ce3bc4beb61c5cfc85d7aa9907fb646
poper-huangdong <dong.huang@poper.co>
2026-09-11 11:33:44 +0800

    Update title version from v7 to v8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}-v7</title>
+        <title>{{ config('app.name', 'Laravel') }}-v8</title>
 
         @fonts
 
